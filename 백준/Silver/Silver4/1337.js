a83fab06a8150a428db07c69ca0be914a39a1ee8
soohyun <bmy1320@gmail.com>
2022-09-29 11:40:33 +0900

    2022-09-29

--- 백준/Silver/Silver4/1337.js
+++ 백준/Silver/Silver4/1337.js
@@ -1,13 +1,25 @@
 const input = require("fs")
-  .readFileSync("././index.txt")
+  .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n");
 
 const T = Number(input[0]);
 const nums = [];
+const dict = {};
 for (let i = 1; i <= T; i++) {
   nums.push(Number(input[i]));
+  dict[Number(input[i])] = 0;
 }
 
-nums.sort((a, b) => a - b);
+let MIN = Infinity;
+for (let i of nums) {
+  for (let j = -4; j <= 4; j++) {
+    let tmp = 0;
+    for (let k = j; k < j + 5; k++) {
+      if (dict[i + k] === 0) tmp += 1;
+    }
+    if (MIN > 5 - tmp) MIN = 5 - tmp;
+  }
+}
+console.log(MIN);
