edf893edefbde99fbbd1a97fd3e3944b7fc58993
soohyun <bmy1320@gmail.com>
2022-10-20 11:20:52 +0900

    2022-10-20

--- 백준/Gold/Gold2/1918.js
+++ 백준/Gold/Gold2/1918.js
@@ -1,18 +1,21 @@
 /* 
-요구사항
+<요구사항>
 1. 우선순위를 구분하는게 핵심일듯하다.
 2. 괄호 내 연산자 구분하기
 3. *,/ 연산이 +.- 연산보다 우선순위이므로 잘 처리
 4. 괄호내 괄호 처리가 까다로워 보임
 
-해결법
-1. 스택을 사용해서 식을 하나씩 확인한다.
-2. 우선순위이니까 부호에 우선순위를 매겨주면 어떨까 싶음
-3. 예를 들어 +,- 는 1, *,/ 는 2 그리고 괄호가 싸이면 어떤 부호든 +2, (+1이면 겹치니까)
-4. 2,3번 과정은 예외상황이 너무 많음. 풀이법 바꾸자.
-5. 열린 괄호와 닫힌 괄호 내의 연산들을 바로바로 붙여주자.
-6. +,- 와 *,/ 를 구분해서 store 배열에 담아두다가 한번에 더하는 식으로 풀기.
-
+<해결법>
+1. 식을 하나씩 확인하여 스택을 사용한다.
+2. 알파벳이 나오면 바로 결과값에 더해준다.
+3. 여는 괄호가 나오면 store 에 저장해둔다.
+4. 닫는 괄호가 나오면 store의 오른쪽 값들 부터 시작해서 
+   여는 괄호가 나올때까지 결과값에 더해준다.
+   이때, 여는 괄호가 나오면 while문을 종료하고 store에서 여는 괄호 삭제.
+5. +,- 연산자는 우선 순위가 낮으므로 괄호내에 있는 상황까지 고려해서 
+   여는 괄호가 나올때까지 앞에 먼저 나온 연산자들을 모두 결과값에 더해준다.
+6. *,/ 연산자는 우선 순위가 높다. 따라서, 앞 쪽에 먼저 나온 *,/ 연산자를 먼저 계산해줘야하므로
+   store에 *,/ 연산자가 저장되어있는지 확인하고 그값들은 결과값에 우선 더해준 뒤에 store에 추가시킨다.
 */
 
 const input = require("fs").readFileSync("/dev/stdin").toString().trim();
@@ -23,24 +26,19 @@ function solution(exp) {
     if (/[A-Z]/.test(v)) result += v;
     else {
       if (v === "(") store.push(v);
-      else if (v === ")") {
-        while (store.length && store[store.length - 1] !== "(") {
+      else {
+        while (store.length) {
+          let storeEndV = store[store.length - 1];
+          if (storeEndV === "(") break;
+          else if (v === "+" || v === "-") {
+            if (storeEndV === "(") break;
+          } else if (v === "*" || v === "/") {
+            if (storeEndV !== "*" && storeEndV !== "/") break;
+          }
           result += store.pop();
         }
-        store.pop();
-      } else if (v === "+" || v === "-") {
-        while (store.length && store[store.length - 1] !== "(") {
-          result += store.pop();
-        }
-        store.push(v);
-      } else if (v === "*" || v === "/") {
-        while (
-          store.length &&
-          (store[store.length - 1] === "*" || store[store.length - 1] === "/")
-        ) {
-          result += store.pop();
-        }
-        store.push(v);
+        if (v === ")") store.pop();
+        else store.push(v);
       }
     }
   });
@@ -49,8 +47,3 @@ function solution(exp) {
 }
 
 console.log(solution(input));
-
-//G*(A-B*(C/D*H+E)/F)
-// GABCD/H*E+*F/-*
-// *(-*(+/)/)
-// *-*
---
+++ 백준/Gold/Gold5/5430.js
@@ -0,0 +1,42 @@
+/*
+요구 사항
+R : 배열에 있는 수의 순서를 뒤집음.
+D : 배열의 첫 번째 수를 버림.
+- 빈 배열에 D를 사용하면 'error' 출력.
+- 주어진 함수를 실행하고 남은 배열이나 error 출력
+
+해결 전략
+우선, R이 두번나오면 원본 배열이니까 이걸 이용해야할듯 하다.
+주어진 함수가 기니까 뒤집으라고 다 뒤집으면 시간초과 날 것이 분명.
+D는 맨 앞자리를 없애는 거라 splice를 사용해야할 것 같은데, pop 을 쓰는 것이
+더 효율적일 것 같으니 처음 주어진 배열을 애초에 뒤집어 넣고 D 가 나오면 pop을 사용하자.
+그리고, 마지막에 출력할 때 다시 뒤집어주자.
+*/
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+for (let i = 1; i <= Number(input[0]); i++) {
+  let [S, N, Nums] = [
+    input[i * 3 - 2],
+    Number(input[i * 3 - 1]),
+    input[i * 3].split(/\D/g).filter((v) => v !== ""),
+  ];
+  let RUR = false;
+  let check = true;
+  if (N === 0) Nums = [];
+  S.split("").map((v) => {
+    if (v === "R") {
+      RUR = !RUR;
+    } else if (v === "D") {
+      if (Nums.length === 0) check = false;
+      if (RUR) Nums.pop();
+      else Nums = Nums.splice(1);
+    }
+  });
+  if (RUR) Nums.reverse();
+  console.log(check === true ? "[" + Nums.join(",") + "]" : "error");
+}
