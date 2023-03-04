f99af339a9a8d27c07eceac9725660cff20bdff4
soohyun <bmy1320@gmail.com>
2023-03-04 23:37:46 +0900

    feat: 문제 풀이 업로드

--- 백준/Gold/Gold3/20366.js
+++ 백준/Gold/Gold3/20366.js
@@ -1,7 +1,23 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+const input = require('fs').readFileSync('././index.txt').toString().trim().split('\n');
 const log = console.log;
 const inputN = Number(input[0]);
 const inputSnows = input[1].split(' ').map(Number);
+
+/*
+3 5 2 5 9
+
+2 3 5 5 9
+
+[
+  [ 5, 0, 1 ],  [ 7, 0, 2 ], [ 7, 0, 3 ],  [ 11, 0, 4 ],
+
+  [ 8, 1, 2 ],  [ 8, 1, 3 ], [ 12, 1, 4 ], 
+
+  [ 10, 2, 3 ], [ 14, 2, 4 ], 
+
+  [ 14, 3, 4 ]
+]
+*/
 
 function solution(N, snows) {
   const snowList = snows.sort((a, b) => a - b);
@@ -13,11 +29,10 @@ function solution(N, snows) {
     }
   }
   list = list.sort((a, b) => a[0] - b[0]);
-
   for (let i = 0; i < list.length - 1; i++) {
     const diff = Math.abs(list[i][0] - list[i + 1][0]);
     if (diff < answer) {
-      if ([...new Set([...list[i].slice(1), ...list[i + 1].slice(1)].map(String))].length === 4) {
+      if (new Set([...list[i].slice(1), ...list[i + 1].slice(1)].map(String)).size === 4) {
         answer = diff;
       }
     }
